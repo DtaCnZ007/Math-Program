afdccce2f00e022860b82eb45d80012880a7f7de
DtaCnZ007 <247421998+DtaCnZ007@users.noreply.github.com>
2025-12-02 22:48:23 +0530

    Added code comments

--- src/nigga.c
+++ src/nigga.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+
+// Creating factorial function
 int factorial(int n){
     int fact=1;
     for(int i=1; i<=n*2; i++){
@@ -7,18 +9,25 @@ int factorial(int n){
     }
     return fact;
 }
+
+// Main function
 int main()
 {
     int n,i,j,fact=1;
     float x,s=1,t=0;
+    // Accepting user input
     printf("Enter the number of terms");
     scanf("%d", &n);
     printf("Enter the value of x:");
     scanf("%d", &x);
+
+    // Computing the result
     for(i=1; i<=n; i++){
       t=(pow(-1,i)*pow(x,2*i))/factorial(n);
       s+=t;
     }
+
+    // Printing the result to terminal
     printf("The sum of the series is:%d", s);
     return 0;
 }
